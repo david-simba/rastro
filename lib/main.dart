26be529a46960b57eca9f4fbc9c243a10fc682b9
cristian-simba <davidsimba200@gmail.com>
2026-01-24 15:18:02 -0500

    refactor: remove riverpod generator and implement dotenv for environments

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rastro/ui/widgets/text/index.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-part 'main.g.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env.dev');
+  MapboxOptions.setAccessToken(dotenv.env['MAPBOX_ACCESS_TOKEN'] ?? '');
 
-@riverpod
-String helloWorld(Ref ref) {
-  return 'Riverpod and Mapbox Configuration';
-}
-
-void main() {
   runApp(
     const ProviderScope(
-      child: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView(),
-    );
-  }
-}
-
-class HomeView extends ConsumerWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final String value = ref.watch(helloWorldProvider);
-
-    return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AppText("Rastro App", align: TextAlign.center, variant: TextVariant.title),
-            SizedBox(height: 10),
-            AppText(value, align: TextAlign.center),
-          ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: Text("Maps"),
+          )
         ),
       ),
-    );
-  }
+    ),
+  );
 }
