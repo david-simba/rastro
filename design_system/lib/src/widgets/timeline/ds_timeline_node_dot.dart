9ae421c92becd9d099b2576cf25b2241d423a46f
David Simba <davidsimba200@gmail.com>
2026-04-06 10:34:18 -0500

    feat(design_system): add listener for timeline node

--- design_system/lib/src/widgets/timeline/ds_timeline_node_dot.dart
+++ design_system/lib/src/widgets/timeline/ds_timeline_node_dot.dart
@@ -5,8 +5,13 @@ enum DsTimelineNodeType { start, mid, end }
 
 class DsTimelineNodeDot extends StatelessWidget {
   final DsTimelineNodeType type;
+  final bool isHighlighted;
 
-  const DsTimelineNodeDot({required this.type, super.key});
+  const DsTimelineNodeDot({
+    required this.type,
+    this.isHighlighted = false,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +40,16 @@ class DsTimelineNodeDot extends StatelessWidget {
         );
       case DsTimelineNodeType.mid:
         return Container(
-          width: 8,
-          height: 8,
-          margin: const EdgeInsets.symmetric(horizontal: 10),
+          width: isHighlighted ? 12 : 8,
+          height: isHighlighted ? 12 : 8,
+          margin: EdgeInsets.symmetric(horizontal: isHighlighted ? 8 : 10),
           decoration: BoxDecoration(
-            color: colors.surface,
+            color: isHighlighted ? DsColors.blue500 : colors.surface,
             shape: BoxShape.circle,
-            border: Border.all(color: colors.border, width: 1.5),
+            border: Border.all(
+              color: isHighlighted ? DsColors.blue500 : colors.border,
+              width: 1.5,
+            ),
           ),
         );
     }
